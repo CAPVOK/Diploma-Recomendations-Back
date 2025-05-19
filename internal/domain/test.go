6b1147d199ef12162e58bf233bca57f84b133c59
SicParv1sMagna <tayushiev@gmail.com>
2025-05-20 00:23:56 +0300

    Added assignee

--- internal/domain/test.go
+++ internal/domain/test.go
@@ -11,6 +11,7 @@ type Test struct {
 	ID          uint   `gorm:"primaryKey;autoIncrement"`
 	Name        string `gorm:"not null;unique"`
 	Description string
+	Assignee    Assignee    `json:"assignee" gorm:"type:varchar(20);not null;assignee IN ('TEACHER', 'RECOMMENDATION');default:'TEACHER'"`
 	Deadline    time.Time   `gorm:"not null"`
 	Courses     []*Course   `gorm:"many2many:course_tests;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Questions   []*Question `gorm:"many2many:test_questions;constraint:OnUpdate:CASCADE;OnDelete:CASCADE;"`
@@ -20,6 +21,7 @@ type TestResponse struct {
 	ID          uint      `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
+	Assignee    string    `json:"assignee"`
 	Deadline    time.Time `json:"deadline"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
@@ -30,11 +32,23 @@ type TestResponseWithQuestions struct {
 	Questions []QuestionResponse `json:"questions"`
 }
 
+type Assignee string
+
+const (
+	Teacher        Assignee = "TEACHER"
+	Recommendation Assignee = "RECOMMENDATION"
+)
+
+func (a Assignee) String() string {
+	return string(a)
+}
+
 func (c *Test) ToTestResponse() TestResponse {
 	return TestResponse{
 		ID:          c.ID,
 		Name:        c.Name,
 		Description: c.Description,
+		Assignee:    c.Assignee.String(),
 		Deadline:    c.Deadline,
 		CreatedAt:   c.CreatedAt,
 		UpdatedAt:   c.UpdatedAt,
